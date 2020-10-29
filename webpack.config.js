8724a76a5b63364a311469dc04b6d60f5e01a9f6
Dinn <ib3smys@gmail.com>
2020-10-29 20:50:51 +0900

    webpack 설치 및 babel-loader 세팅
    * npm init
    * webpack 설치
    * babel-loader 설치
    * preset-env, preset-react 세팅

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
